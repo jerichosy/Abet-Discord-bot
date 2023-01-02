fff314c346cefa6d24a1fd764814d2c77711ec67
Matthew Jericho Sy <69558553+jerichosy@users.noreply.github.com>
2023-01-03 03:24:09 +0800

    Reorganize/add comments in reposters

--- main.py
+++ main.py
@@ -214,9 +214,12 @@ async def on_message(message):
                     await message.channel.send(random.choice(mhy_response))
                 break
 
+    # --- REPOSTERS ---
+    # TODO: Either try to make it consistent across or think of a better/flexible/DRY solution
+    # FIXME: checking is only arbitrarily implemented
+
     # Made redundant by QuickVids bot
-    # # TODO: Either try to make it consistent across or think of a better/flexible/DRY solution
-    # # Error checking is only arbitarily implemented
+    # FIXME: This is not up to date with the FB and IG reel reposters
     # TIKTOK_REGEX = r"(https?://www\.tiktok\.com/(?:embed|@(?P<user_id>[\w\.-]+)/video)/(?P<id>\d+))"
     # TIKTOK_VM_REGEX = r"(https?://(?:vm|vt)\.tiktok\.com/\w+)"
     # tiktok_url = re.findall(TIKTOK_REGEX, message.content)
