af0a8679ec4d76e554323ae632ae3fe79d7d3d4c
Matthew Jericho Sy <69558553+jerichosy@users.noreply.github.com>
2023-03-11 05:15:23 +0800

    Rewrote ChatGPT cmd to do async requests

--- cogs/OpenAI.py
+++ cogs/OpenAI.py
@@ -1,9 +1,10 @@
+import json
+import os
+
 import aiohttp
 import discord
-import openai
 from discord.ext import commands
 from dotenv import load_dotenv
-from openai.error import RateLimitError
 
 from cogs.utils.ExchangeRateUSDPHP import ExchangeRateUSDPHP
 
@@ -21,50 +22,61 @@ class OpenAI(commands.Cog):
 
         # print(prompt)
         async with ctx.typing():
-            try:
-                response = openai.ChatCompletion.create(
-                    model="gpt-3.5-turbo",
-                    messages=[
-                        # {
-                        #     "role": "system",
-                        #     "content": "You must respond in 2000 characters or less",
-                        # },
-                        {"role": "user", "content": prompt},
-                    ],
-                )
-            except RateLimitError:
-                await ctx.send(
-                    "Uh oh, looks like <@298454523624554501> forgot to pay the bills. No ChatGPT for now..."
-                )
-            else:
-                answer = response["choices"][0]["message"]["content"]
-                # print("Length: ", len(answer))
+            headers = {
+                "Authorization": f"Bearer {os.getenv('OPENAI_API_KEY')}",
+                "Content-Type": "application/json",
+            }
+            data = {
+                "model": "gpt-3.5-turbo",
+                "messages": [
+                    # {"role": "system", "content": "You must respond in 2000 characters or less"},
+                    {"role": "user", "content": prompt},
+                ],
+            }
+            async with aiohttp.ClientSession() as session:
+                async with session.post(
+                    "https://api.openai.com/v1/chat/completions",
+                    headers=headers,
+                    data=json.dumps(data),
+                ) as resp:
+                    response = await resp.json()
+                    print(resp.status)
+                    if resp.status != 200:
+                        if resp.status == 500:
+                            return await ctx.send(
+                                "The server had an error while processing your request. Please try again."
+                            )
+                        else:
+                            return await ctx.send(
+                                f"Uh oh, looks like <@298454523624554501> needs to take a look at this:\n> {response['error']['message']}"
+                            )
 
-                print(await currency_USD_PHP.latest_exchange_rate())
-                # Calculate token cost
-                # gpt-3.5-turbo	    $0.002 / 1K tokens
-                total_tokens = response["usage"]["total_tokens"]
-                cost_in_USD = (total_tokens * 0.002) / 1000
-                cost_in_PHP = (
-                    cost_in_USD * await currency_USD_PHP.latest_exchange_rate()
-                )
-                print(cost_in_USD, cost_in_PHP)
+            answer = response["choices"][0]["message"]["content"]
+            # print("Length: ", len(answer))
 
-                # Send response
-                embed = discord.Embed(color=0x2B2D31)
-                embed.set_footer(
-                    text=f"Tokens used: {total_tokens} | Cost: ₱{round(cost_in_PHP, 3)}"
-                )
-                # If answer is long, truncate it and inform in embed
-                if len(answer) > 2000:
-                    answer = answer[:1996] + " ..."
-                    embed.title = "Truncated due to 2000 character limit"
+            print(await currency_USD_PHP.latest_exchange_rate())
+            # Calculate token cost
+            # gpt-3.5-turbo	    $0.002 / 1K tokens
+            total_tokens = response["usage"]["total_tokens"]
+            cost_in_USD = (total_tokens * 0.002) / 1000
+            cost_in_PHP = cost_in_USD * await currency_USD_PHP.latest_exchange_rate()
+            print(cost_in_USD, cost_in_PHP)
 
-                # print("Truncated length: ", len(answer))
+            # Send response
+            embed = discord.Embed(color=0x2B2D31)
+            embed.set_footer(
+                text=f"Tokens used: {total_tokens} | Cost: ₱{round(cost_in_PHP, 3)}"
+            )
+            # If answer is long, truncate it and inform in embed
+            if len(answer) > 2000:
+                answer = answer[:1996] + " ..."
+                embed.title = "Truncated due to 2000 character limit"
 
-                await ctx.send(answer, embed=embed)
+            # print("Truncated length: ", len(answer))
 
-                # print(response)
+            await ctx.send(answer, embed=embed)
+
+            # print(response)
 
 
 async def setup(bot):
