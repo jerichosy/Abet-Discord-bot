ddb1702792354c70ad4c87c3263401f3820fc70c
Matthew Jericho Sy <69558553+jerichosy@users.noreply.github.com>
2023-03-12 06:15:22 +0800

    Improved comparison of time since last update in ExchangeRateUSDPHP class

--- cogs/utils/ExchangeRateUSDPHP.py
+++ cogs/utils/ExchangeRateUSDPHP.py
@@ -21,7 +21,7 @@ class ExchangeRateUSDPHP:
         # self.exchange_rate = response['php']
 
     async def check_latest(self):
-        if time.time() - self.last_updated > 86400:
+        if time.time() - self.last_updated >= 86400:
             await self.update()
 
     async def latest_exchange_rate(self):
