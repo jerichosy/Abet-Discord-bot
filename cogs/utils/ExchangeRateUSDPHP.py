2028cdd5d37bb6f7d9ccfc8c8bc9da1303c00066
Matthew Jericho Sy <69558553+jerichosy@users.noreply.github.com>
2023-03-12 21:15:42 +0800

    Apply private (naming) conventions to ExchangeRateUSDPHP class

--- cogs/utils/ExchangeRateUSDPHP.py
+++ cogs/utils/ExchangeRateUSDPHP.py
@@ -6,24 +6,22 @@ import aiohttp
 
 class ExchangeRateUSDPHP:
     def __init__(self):
-        self.last_updated = time.time()
-        self.loop = asyncio.get_event_loop()
-        self.loop.create_task(self.update())
+        self._last_updated = time.time()
+        self._loop = asyncio.get_event_loop()
+        self._loop.create_task(self._update())
 
-    async def update(self):
+    async def _update(self):
         async with aiohttp.ClientSession() as session:
             async with session.get(
                 "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/php.json"
             ) as resp:
                 if resp.status == 200:
-                    self.exchange_rate = (await resp.json())["php"]
-        # self.date = response['date']
-        # self.exchange_rate = response['php']
+                    self._exchange_rate = (await resp.json())["php"]
 
-    async def check_latest(self):
-        if time.time() - self.last_updated >= 86400:
-            await self.update()
+    async def _check_latest(self):
+        if time.time() - self._last_updated >= 86400:
+            await self._update()
 
     async def latest_exchange_rate(self):
-        await self.check_latest()
-        return self.exchange_rate
+        await self._check_latest()
+        return self._exchange_rate
