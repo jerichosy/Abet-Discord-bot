147b1b466701c3d8f6572c476d03cb7c6a322cb4
Matthew Jericho Sy <69558553+jerichosy@users.noreply.github.com>
2023-03-26 05:12:44 +0800

    Added Get Raw String context menu

--- cogs/Tools.py
+++ cogs/Tools.py
@@ -24,6 +24,21 @@ class PDFFlags(commands.FlagConverter, prefix="--", delimiter=""):
 class Tools(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.ctx_menu = app_commands.ContextMenu(
+            name="Get Raw String",
+            callback=self.get_raw_string,
+        )
+        self.bot.tree.add_command(self.ctx_menu)
+
+    async def cog_unload(self) -> None:
+        self.bot.tree.remove_command(self.ctx_menu.name, type=self.ctx_menu.type)
+
+    async def get_raw_string(
+        self, interaction: discord.Interaction, message: discord.Message
+    ):
+        await interaction.response.send_message(
+            file=discord.File(io.BytesIO(message.content.encode()), "output.txt")
+        )
 
     @staticmethod
     def coin_flip():
