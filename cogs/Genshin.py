5c0bd393bb281724fbc3bfb99782d41ed10bcf5a
Matthew Jericho Sy <69558553+jerichosy@users.noreply.github.com>
2023-04-06 01:12:54 +0800

    Move Genshin cog constants to utils

--- cogs/Genshin.py
+++ cogs/Genshin.py
@@ -5,6 +5,8 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
+from cogs.utils import genshinutils
+
 
 class Genshin(commands.Cog):
     def __init__(self, bot):
@@ -18,37 +20,14 @@ class Genshin(commands.Cog):
         """Genshin Impact: Views current Paimon's Bargains items for the month"""
         current_month = datetime.now().month
 
-        MONTHS = [
-            "Jan",
-            "Feb",
-            "Mar",
-            "Apr",
-            "May",
-            "Jun",
-            "Jul",
-            "Aug",
-            "Sep",
-            "Oct",
-            "Nov",
-            "Dec",
-        ]
-        CHARACTERS = [
-            "Fischl Xiangling",
-            "Beidou Noelle",
-            "Ningguang Xingqiu",
-            "Razor Amber",
-            "Bennett Lisa",
-            "Barbara Kaeya",
-        ]
-
         def display_future():
             built = "\n\n**Future:**\n"
             for x in range(1, 7):
                 built += (
                     "`"
-                    + MONTHS[((current_month + x) % 12) - 1]
+                    + genshinutils.MONTHS[((current_month + x) % 12) - 1]
                     + "` | "
-                    + CHARACTERS[((current_month + x) % 6) - 1]
+                    + genshinutils.CHARACTERS[((current_month + x) % 6) - 1]
                     + "\n"
                 )
 
@@ -60,9 +39,9 @@ class Genshin(commands.Cog):
         await ctx.send(
             "**Current:**\n"
             + "`"
-            + MONTHS[current_month - 1]
+            + genshinutils.MONTHS[current_month - 1]
             + "` | "
-            + CHARACTERS[(current_month % 6) - 1]
+            + genshinutils.CHARACTERS[(current_month % 6) - 1]
             + determine_weapon_series()
             + display_future()
         )
@@ -77,105 +56,12 @@ class Genshin(commands.Cog):
     ):
         """Genshin Impact: Calculate how much Hero's Wit, etc. you need to level/ascend your characters"""
 
-        CHARACTER_EXP_LIST = [
-            0,
-            1000,
-            1325,
-            1700,
-            2150,
-            2625,
-            3150,
-            3725,
-            4350,
-            5000,
-            5700,
-            6450,
-            7225,
-            8050,
-            8925,
-            9825,
-            10750,
-            11725,
-            12725,
-            13775,
-            14875,
-            16800,
-            18000,
-            19250,
-            20550,
-            21875,
-            23250,
-            24650,
-            26100,
-            27575,
-            29100,
-            30650,
-            32250,
-            33875,
-            35550,
-            37250,
-            38975,
-            40750,
-            42575,
-            44425,
-            46300,
-            50625,
-            52700,
-            54775,
-            56900,
-            59075,
-            61275,
-            63525,
-            65800,
-            68125,
-            70475,
-            76500,
-            79050,
-            81650,
-            84275,
-            86950,
-            89650,
-            92400,
-            95175,
-            98000,
-            100875,
-            108950,
-            112050,
-            115175,
-            118325,
-            121525,
-            124775,
-            128075,
-            131400,
-            134775,
-            138175,
-            148700,
-            152375,
-            156075,
-            159825,
-            163600,
-            167425,
-            171300,
-            175225,
-            179175,
-            183175,
-            216225,
-            243025,
-            273100,
-            306800,
-            344600,
-            386950,
-            434425,
-            487625,
-            547200,
-        ]
-
         # validation
         if current_level >= target_level:
             return await interaction.response.send_message(
                 "Already achieved the target level."
             )
-        if CHARACTER_EXP_LIST[current_level] < current_experience:
+        if genshinutils.CHARACTER_EXP_LIST[current_level] < current_experience:
             return await interaction.response.send_message(
                 "Current experience exceeds the maximum experience in the current level."
             )
@@ -184,9 +70,11 @@ class Genshin(commands.Cog):
 
         for i in range(current_level, target_level):
             if i == current_level:
-                totalExpNeeded += CHARACTER_EXP_LIST[i] - current_experience
+                totalExpNeeded += (
+                    genshinutils.CHARACTER_EXP_LIST[i] - current_experience
+                )
             else:
-                totalExpNeeded += CHARACTER_EXP_LIST[i]
+                totalExpNeeded += genshinutils.CHARACTER_EXP_LIST[i]
 
         def ceilNumber(num, place):
             div = 1
