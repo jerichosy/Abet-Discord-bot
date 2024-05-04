b739341077c22f958086727dc245ca18183ac570
Matthew Jericho Sy <69558553+jerichosy@users.noreply.github.com>
2024-05-05 01:22:19 +0800

    Do not use quotation marks when printing quotes in Discord in all quote cmds

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -155,7 +155,7 @@ class Fun(commands.Cog):
         embed = discord.Embed(description=f"**{member.mention} quotes:**\n")
         for quote in quotes:
             embed.add_field(
-                name="", value=f"**{quote.id}**: '{quote.quote}'", inline=False
+                name="", value=f"**{quote.id}**: {quote.quote}", inline=False
             )
         return embed
 
