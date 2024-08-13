42526443a4ed1a8d468eb58e19361f9832d0b2c7
Matthew Jericho Sy <69558553+jerichosy@users.noreply.github.com>
2024-08-13 19:03:30 +0800

    Refactor database code to allow clean adding of new schemas

--- models/db.py
+++ models/db.py
@@ -1,40 +1,45 @@
 from typing import Sequence
 
 from sqlalchemy import delete, func, select
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
 from sqlalchemy.ext.asyncio.session import async_sessionmaker
 
 from .schema import Base, Quote
 
 
-class QuotesDB:
-    def __init__(self, uri: str) -> None:
-        self.engine = create_async_engine(uri, echo=True)
-        self.session = async_sessionmaker(self.engine, expire_on_commit=False)
+class BaseDBManager:
+    def __init__(self, engine: AsyncEngine) -> None:
+        self.engine = engine
+        self.SessionLocal = async_sessionmaker(bind=self.engine, class_=AsyncSession, expire_on_commit=False)
 
     async def _create_tables(self) -> None:
         async with self.engine.begin() as conn:
             await conn.run_sync(Base.metadata.create_all)
 
     async def _insert_objects(self, objects: list[Base]):
-        async with self.session() as session:
+        async with self.SessionLocal() as session:
             async with session.begin():
                 session.add_all(objects)
 
+    async def close(self) -> None:
+        await self.engine.dispose()
+
+
+class QuotesManager(BaseDBManager):
     async def find_if_quote_exists_by_quote(self, quote: str, member_id: int) -> Quote | None:
-        async with self.session() as session:
+        async with self.SessionLocal() as session:
             stmt = select(Quote).where(Quote.quote.ilike(quote)).where(Quote.quote_by.ilike(str(member_id)))
             result = await session.execute(stmt)
             return result.scalar_one_or_none()
 
     async def find_if_quote_exists_by_id(self, quote_id: int) -> Quote | None:
-        async with self.session() as session:
+        async with self.SessionLocal() as session:
             stmt = select(Quote).where(Quote.quote_by.ilike(str(quote_id)))
             result = await session.execute(stmt)
             return result.scalar_one_or_none()
 
     async def find_random_quote(self, member_id: int) -> Quote | None:
-        async with self.session() as session:
+        async with self.SessionLocal() as session:
             stmt = select(Quote).where(Quote.quote_by.ilike(str(member_id))).order_by(func.random()).limit(1)
             result = await session.execute(stmt)
             return result.scalar_one_or_none()
@@ -48,7 +53,7 @@ class QuotesDB:
         return new_quote.id
 
     async def find_quotes_by_member_id(self, quote_by: int, page: int, per_page: int) -> Sequence[Quote]:
-        async with self.session() as session:
+        async with self.SessionLocal() as session:
             stmt = (
                 select(Quote)
                 .where(Quote.quote_by.ilike(str(quote_by)))
@@ -60,16 +65,13 @@ class QuotesDB:
             return result.scalars().all()
 
     async def find_quote_by_id(self, quote_id: int) -> Quote | None:
-        async with self.session() as session:
+        async with self.SessionLocal() as session:
             stmt = select(Quote).where(Quote.id == quote_id)
             result = await session.execute(stmt)
             return result.scalar_one_or_none()
 
     async def delete_quote_by_id(self, quote_id: int) -> None:
-        async with self.session() as session:
+        async with self.SessionLocal() as session:
             async with session.begin():
                 stmt = delete(Quote).where(Quote.id == quote_id)
                 await session.execute(stmt)
-
-    async def close(self) -> None:
-        await self.engine.dispose()
---
+++ models/engine.py
@@ -0,0 +1,11 @@
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+
+
+class EngineSingleton:
+    _engine: AsyncEngine = None
+
+    @classmethod
+    def get_engine(cls, uri: str) -> AsyncEngine:
+        if cls._engine is None:
+            cls._engine = create_async_engine(uri, echo=True)
+        return cls._engine
