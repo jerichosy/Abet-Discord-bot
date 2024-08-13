303ddca9964d36b6acee67f45ddd05e05cef0a4a
Matthew Jericho Sy <69558553+jerichosy@users.noreply.github.com>
2024-08-14 00:43:31 +0800

    feat(tag cmds): basic implementation
    Has been thoroughly tested. More UX improvements possible.

--- models/db.py
+++ models/db.py
@@ -4,7 +4,7 @@ from sqlalchemy import delete, func, select
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
 from sqlalchemy.ext.asyncio.session import async_sessionmaker
 
-from .schema import Base, Quote
+from .schema import Base, Quote, Tag
 
 
 class BaseDBManager:
@@ -68,4 +68,29 @@ class QuotesManager(BaseDBManager):
         async with self.SessionLocal() as session:
             async with session.begin():
                 stmt = delete(Quote).where(Quote.id == quote_id)
+                await session.execute(stmt)
+
+
+class TagsManager(BaseDBManager):
+    async def find_tag_by_name(self, name: str) -> Tag | None:
+        async with self.SessionLocal() as session:
+            stmt = select(Tag).where(Tag.name.ilike(name))
+            result = await session.execute(stmt)
+            return result.scalar_one_or_none()
+
+    async def insert_tag(self, name: str, content: str, owner: int) -> int:
+        new_tag = Tag(name=name, content=content, owner=str(owner))
+        await self._insert_objects([new_tag])
+        return new_tag.id
+
+    async def find_tags_by_user_id(self, owner: int) -> Sequence[Tag]:
+        async with self.SessionLocal() as session:
+            stmt = select(Tag).where(Tag.owner.ilike(str(owner))).order_by(Tag.id)
+            result = await session.execute(stmt)
+            return result.scalars().all()
+
+    async def delete_tag_by_name(self, name: str) -> None:
+        async with self.SessionLocal() as session:
+            async with session.begin():
+                stmt = delete(Tag).where(Tag.name == name)
                 await session.execute(stmt)
