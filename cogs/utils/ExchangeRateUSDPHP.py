ab5a59f97a494075ec6c353b541be4a70c8ac651
Matthew Jericho Sy <69558553+jerichosy@users.noreply.github.com>
2024-08-26 09:05:07 +0800

    refactor/fix(ExchangeRateUSDPHP): Lazy initialize and fix potential bugs

--- cogs/utils/ExchangeRateUSDPHP.py
+++ cogs/utils/ExchangeRateUSDPHP.py
@@ -1,7 +1,5 @@
-import asyncio
+# import asyncio
 import time
-
-import aiohttp
 
 UPDATE_INTERVAL = 86400  # 24 hours
 
@@ -10,9 +8,9 @@ class ExchangeRateUSDPHP:
     def __init__(self, session):
         self._session = session
         self._exchange_rate = None
-        self._last_updated = time.time()
-        self._loop = asyncio.get_event_loop()
-        self._loop.create_task(self._update())
+        self._last_updated = None
+        # self._loop = asyncio.get_event_loop()
+        # self._loop.create_task(self._update())
 
     async def _update(self):
         try:
@@ -22,14 +20,20 @@ class ExchangeRateUSDPHP:
                 resp.raise_for_status()
                 self._exchange_rate = (await resp.json())["usd"]["php"]
                 self._last_updated = time.time()
+                print(f"Updated exchange rate: {self._exchange_rate}")
         except Exception as e:
             print(f"Failed to update exchange rate: {e}")
             raise e
 
     async def _check_latest(self):
-        if time.time() - self._last_updated >= UPDATE_INTERVAL:
+        if self._exchange_rate is None:
+            print("No exchange rate found, updating...")
+            await self._update()
+        elif time.time() - self._last_updated >= UPDATE_INTERVAL:
+            print("Exchange rate is outdated, updating...")
             await self._update()
 
     async def latest_exchange_rate(self):
         await self._check_latest()
+        print(f"Returning exchange rate: {self._exchange_rate}")
         return self._exchange_rate
