4e4e3f5c9888a19287a12621507429b4c465fa56
Matthew Jericho Sy <69558553+jerichosy@users.noreply.github.com>
2024-08-29 00:52:36 +0800

    Restrict OpenAI cmds to owners only
    While it is not possible to obtain statistics on historical costs associated with allowing public usage of OpenAI cmds, well let's just say I want to keep my monthly costs down (in everything). Hence, this commit.
    
    Maybe, I'll revert this once I'm sure I'm capable of paying for the costs with lots of extras on hand.

--- cogs/AI.py
+++ cogs/AI.py
@@ -79,7 +79,7 @@ class AI(commands.Cog):
     @app_commands.describe(image="Use GPT-4 Vision model to allow images as input and answer questions about them")
     @app_commands.describe(model="Defaults to GPT-4 (ChatGPT Plus) but can be specified to use GPT-3.5 (ChatGPT)")
     @app_commands.describe(response='Defaults to "Embed" but can be changed to "Message" for easy copying on mobile')
-    # @commands.is_owner()
+    @commands.is_owner()  # If we allow everyone again, in OpenAI API Platform, set up a proj in the default org with its own API key so we can track costs specific to Abet bot's OpenAI API usage
     async def chatgpt(
         self,
         ctx,
@@ -325,6 +325,7 @@ class AI(commands.Cog):
     @commands.cooldown(rate=1, per=8, type=commands.BucketType.user)
     @commands.max_concurrency(number=1, per=commands.BucketType.user, wait=False)
     @app_commands.describe(audio_file="Supports MP3, MP4, MPEG, MPGA, M4A, WAV, and WEBM. Limited to 25 MB.")
+    @commands.is_owner()  # If we allow everyone, in OpenAI API Platform, set up a proj in the default org with its own API key so we can track costs specific to Abet bot's OpenAI API usage
     async def whisper(self, interaction: discord.Interaction, audio_file: discord.Attachment):
         """Uses OpenAI's Whisper model to transcribe audio (speech) to text"""
 
