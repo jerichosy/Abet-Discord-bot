d0cc4b7d824d30a8aa71aa4ad33e064961355bc1
Matthew Jericho Sy <69558553+jerichosy@users.noreply.github.com>
2024-09-01 19:28:16 +0800

    feat(wai stop-gen cmd): Add challenge for Wai to prevent stoppage of generation

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -1,6 +1,9 @@
 import asyncio
+import mimetypes
 import os
 import random
+import string
+import time
 from io import BytesIO
 from typing import List, Literal
 
@@ -18,6 +21,24 @@ WAIFU_IM_API_HEADERS = {"Accept-Version": "v6"}
 class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.riddles = [
+            (
+                "What comes but never arrives?",
+                "tomorrow",
+            ),
+            (
+                "I have keys but no locks. I have space but no room. You can enter, but you can't go outside. What am I?",
+                "keyboard",
+            ),
+            (
+                "What has words but never speaks?",
+                "book",
+            ),
+            (
+                "I speak without a mouth and hear without ears. I have no body, but I come alive with wind. What am I?",
+                "echo",
+            ),
+        ]
 
     # Don't make this into an embed
     @commands.hybrid_command(aliases=["fuckcarl"])
@@ -124,20 +145,209 @@ class Fun(commands.Cog):
     # TODO: make this work as hybrid cmds
     waikei_group = Group(name="waikei", description="Waikei commands")
 
+    async def common_check(self, channel_id):
+        def predicate(m):
+            return m.channel.id == channel_id and m.author.id == self.bot.WAIKEI_USER.id
+            # return m.channel.id == channel_id and m.author.id == 298454523624554501  # for testing w/ kyoya1101
+            # return m.channel.id == channel_id and m.author.id == 249355534204010506  # for testing w/ notabamf
+            # return m.channel.id == channel_id and m.author.id == 199017953922908160  # for testing w/ hemeduhh
+            # return m.channel.id == channel_id and m.author.id == 982369011494957118  # for testing w/ 7emadu
+
+        return predicate
+
+    async def cancel_check(self, interaction):
+        await interaction.response.send_message(
+            f"Hey <@{self.bot.WAIKEI_USER.id}>, if you do not respond with 'cancel' in 5 seconds, the command will proceed."
+        )
+        check_message = await self.common_check(interaction.channel_id)
+        return await self.bot.wait_for(
+            "message", timeout=5.0, check=lambda m: check_message(m) and m.content.lower() == "cancel"
+        )
+
+    async def reaction_check(self, interaction):
+        def check_reaction(reaction, user):
+            return (
+                reaction.message.channel.id == interaction.channel_id
+                and user.id == self.bot.WAIKEI_USER.id
+                # and user.id == 298454523624554501  # for testing w/ kyoya1101
+                # and user.id == 249355534204010506  # for testing w/ notabamf
+                # and user.id == 199017953922908160  # for testing w/ hemeduhh
+                # and user.id == 982369011494957118  # for testing w/ 7emadu
+                and str(reaction.emoji) == "👍"
+            )
+
+        await interaction.channel.send(
+            "Are you sure you want to cancel? React to this message with a 👍 emoji within 5 seconds to confirm."
+        )
+        return await self.bot.wait_for("reaction_add", timeout=5.0, check=check_reaction)
+
+    async def password_check(self, interaction):
+        await interaction.channel.send(
+            "Ok. But first, create a password that includes an uppercase letter, lowercase letter, number, special character, and is at least 10 characters long with no spaces within 15 seconds."
+        )
+        check_message = await self.common_check(interaction.channel_id)
+        # timeout 15.0 (current) or 29.0 as testing showed that the upper-bound (w/ multiple attempts) can be 25 secs and we want them to experience the rest of the challenge without giving up in the early-middle
+        # but 15.0 so that they have to at least go through it thrice (third time's a charm)
+        return await self.bot.wait_for(
+            "message", timeout=15.0, check=lambda m: check_message(m) and self.check_password_criteria(m.content)[-1]
+        )
+
+    async def palindrome_password_check(self, interaction):
+        await interaction.channel.send(
+            "Now, make your password a palindrome while still meeting the criteria, within 15 seconds."
+        )
+        check_message = await self.common_check(interaction.channel_id)
+        return await self.bot.wait_for(
+            "message",
+            timeout=15.0,  # FIXME: adjust timeout
+            check=lambda m: check_message(m)
+            and self.is_palindrome(m.content)
+            and self.check_password_criteria(m.content)[-1],
+        )
+
+    async def water_emoji_memegen(self, palindrome_msg_content):
+        body = """
+            {
+            "background": "https://64.media.tumblr.com/3d86c59d3adfa6cf085c35e0cf2659b9/tumblr_inline_p20k7xuTgK1qckp08_540.jpg",
+            "style": "string",
+            "text": [
+                "yourpasswordhere"
+            ],
+            "layout": "string",
+            "font": "string",
+            "extension": "string",
+            "redirect": true
+        }
+        """
+        body = body.replace("yourpasswordhere", palindrome_msg_content)
+
+        async with self.bot.session.request(
+            method="POST",
+            url="https://api.memegen.link/templates/custom",
+            headers={"accept": "application/json", "Content-Type": "application/json"},
+            data=body,
+        ) as response:
+            # TODO: In here, we can instead grab the `response.url` and have that link as part of the image which Discord will auto-embed. Check if this will be faster (and if we want the speed UX-wise.)
+            response.raise_for_status()
+            return BytesIO(await response.read()), mimetypes.guess_extension(response.headers.get("Content-Type"))
+
+    async def water_emoji_check(self, interaction, palindrome_msg_content):
+        try:
+            file, ext = await self.water_emoji_memegen(palindrome_msg_content)
+            dfile = discord.File(file, f"password_on_fire{ext}")
+        except Exception as e:
+            dfile = None
+            print(e)
+        await interaction.channel.send(
+            f"Great... Now, your password is on fire 🔥! Type as many 💧 emojis as the length of your password to put it out within 25 seconds.",
+            file=dfile,
+        )
+        check_message = await self.common_check(interaction.channel_id)
+        return await self.bot.wait_for(
+            "message",
+            timeout=25.0,
+            check=lambda m: check_message(m) and m.content.count("💧") == len(palindrome_msg_content),
+        )
+
+    async def math_question_check(self, interaction):
+        question, answer = self.generate_math_question()
+        await interaction.channel.send(
+            f"Lastly, solve this simple math question in 10 seconds: ||{question}|| (unhide it first)"
+        )
+        check_message = await self.common_check(interaction.channel_id)
+        return await self.bot.wait_for(
+            "message", timeout=10.0, check=lambda m: check_message(m) and m.content == str(answer)
+        )
+
+    async def riddle_check(self, interaction):
+        riddle, riddle_answer = random.choice(self.riddles)
+        await interaction.channel.send(f"Oops, one more thing... Here's a riddle for you to answer in 10 seconds: {riddle}")
+        check_message = await self.common_check(interaction.channel_id)
+        return await self.bot.wait_for(
+            "message", timeout=10.0, check=lambda m: check_message(m) and m.content.lower() == riddle_answer
+        )
+
+    def check_password_criteria(self, text):
+        has_upper = any(c.isupper() for c in text)
+        has_lower = any(c.islower() for c in text)
+        has_digit = any(c.isdigit() for c in text)
+        has_special = any(c in string.punctuation for c in text)
+        is_long_enough = len(text) >= 10
+        no_spaces = " " not in text
+        return (
+            has_upper,
+            has_lower,
+            has_digit,
+            has_special,
+            is_long_enough,
+            no_spaces,
+            all([has_upper, has_lower, has_digit, has_special, is_long_enough, no_spaces]),
+        )
+
+    # FIXME: Doesn't check for capitalization
+    def is_palindrome(self, text):
+        cleaned_text = "".join(c for c in text if c.isalnum()).lower()
+        return cleaned_text == cleaned_text[::-1]
+
+    def generate_math_question(self):
+        num1 = random.randint(1, 10)
+        num2 = random.randint(1, 10)
+        num3 = random.randint(1, 10)
+        operation1 = random.choice(["+", "-", "*"])
+        operation2 = random.choice(["+", "-", "*"])
+        question = f"{num1} {operation1} {num2} {operation2} {num3}"
+        answer = eval(question)
+        return question, answer
+
     @waikei_group.command(name="stop-generating")
     async def stop_generating(self, interaction):
-        async with self.bot.session.post(f"{os.getenv('YT_DLP_MICROSERVICE')}/waikei-pretrained/stop-generating") as resp:
-            # Print status line
-            msg = [f"HTTP/{resp.version.major}.{resp.version.minor} {resp.status} {resp.reason}"]
+        """Stop the generation of Waikei LLM"""
 
-            # Print headers
-            for key, value in resp.headers.items():
-                msg.append(f"{key}: {value}")
+        try:
+            timestamps = []
 
-            # Print content
-            msg.append(f"\n{await resp.text()}")
+            timestamps.append(time.time())
+            await self.cancel_check(interaction)  # Challenge 1
+            timestamps.append(time.time())
+            await self.reaction_check(interaction)  # Challenge 2
+            timestamps.append(time.time())
+            await self.password_check(interaction)  # Challenge 3
+            timestamps.append(time.time())
+            palindrome_msg = await self.palindrome_password_check(interaction)  # Challenge 4
+            timestamps.append(time.time())
+            await self.water_emoji_check(interaction, palindrome_msg.content)  # Challenge 5
+            timestamps.append(time.time())
+            await self.math_question_check(interaction)  # Challenge 6
+            timestamps.append(time.time())
+            await self.riddle_check(interaction)  # Challenge 7
+            timestamps.append(time.time())
 
-            await interaction.response.send_message("\n".join(msg))
+            await interaction.followup.send(
+                f"Unfortunately, all challenges were completed successfully, <@{self.bot.WAIKEI_USER.id}> is now unstoppable!"
+            )
+        except asyncio.TimeoutError:
+            await interaction.followup.send(
+                f"<@{self.bot.WAIKEI_USER.id}> didn't complete the challenge in time. The command proceeds."
+            )
+
+            async with self.bot.session.post(
+                f"{os.getenv('YT_DLP_MICROSERVICE')}/waikei-pretrained/stop-generating"
+            ) as resp:
+                # Print status line
+                msg = [f"HTTP/{resp.version.major}.{resp.version.minor} {resp.status} {resp.reason}"]
+
+                # Print headers
+                for key, value in resp.headers.items():
+                    msg.append(f"{key}: {value}")
+
+                # Print content
+                msg.append(f"\n{await resp.text()}")
+
+                await interaction.channel.send("Stopping generation of Waikei LLM...\n```\n{}\n```".format("\n".join(msg)))
+        finally:
+            for i in range(1, len(timestamps)):
+                elapsed_time = timestamps[i] - timestamps[i - 1]
+                print(f"Time elapsed for challenge {i}: {elapsed_time} seconds")
 
     async def get_waifu_im_embed(self, type, category):
         type = "false" if type == "sfw" else "true"
