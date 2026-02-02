1beb36795840e893dd1b209e816086dc64abfbdf
jerichosy <mgsy1101@gmail.com>
2026-02-03 04:22:08 +0800

    feat(scripts): Make scripts simpler

--- scripts/logsdocker.sh
+++ scripts/logsdocker.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-root=$(git rev-parse --show-toplevel)
-compose_file="${root}/docker-compose.yml"
-
 while true
 do
-        docker compose -f ${compose_file} logs --follow -t
+        docker compose logs --follow -t
         echo "Detached from logs. Re-attaching in 5 seconds..."
         echo "Press Ctrl+C to cancel."
         sleep 5
--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-compose_file="/home/jericho/Abet-Discord-bot/docker-compose.yml"
-
 restart() {
-        docker compose -f $compose_file down
-        docker compose -f $compose_file up -d
+        docker compose down
+        docker compose up -d
 }
 
 restart
